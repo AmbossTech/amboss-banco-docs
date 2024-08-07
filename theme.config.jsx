82cfc450eb71ef81c59d1b625d46c21fca3fa01e
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-08-07 13:46:57 -0600

    chore: name change

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { useConfig } from "nextra-theme-docs";
 
 export default {
-  logo: <b>MiBanco</b>,
+  logo: <b>BancoLibre</b>,
   docsRepositoryBase: "https://github.com/AmbossTech/amboss-banco-docs",
   project: {
     link: "https://github.com/AmbossTech/",
@@ -11,7 +11,7 @@ export default {
     const { asPath } = useRouter();
     if (asPath !== "/") {
       return {
-        titleTemplate: "%s – MiBanco",
+        titleTemplate: "%s – BancoLibre",
       };
     }
   },
@@ -26,7 +26,7 @@ export default {
       <>
         <link rel="icon" href="/docs/favicon.ico" type="image/ico" />
         <meta property="og:url" content={url} />
-        <meta property="og:title" content={frontMatter.title || "MiBanco"} />
+        <meta property="og:title" content={frontMatter.title || "BancoLibre"} />
         <meta
           property="og:description"
           content={frontMatter.description || "Banking for the Unbanked"}
@@ -39,7 +39,7 @@ export default {
       <span>
         AGPLv3 {new Date().getFullYear()} ©{" "}
         <a href="https://mibanco.app" target="_blank">
-          MiBanco
+          BancoLibre
         </a>
         .
       </span>
